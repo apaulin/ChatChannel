f55fd47ddd69c7619dcebe2a3bb774a3fad6a0aa
Andre-Claude <andre.claude.paulin@gmail.com>
2018-02-23 16:49:51 -0500

    Adding DB stubs

---
+++ websocketserver/dbase_api_obj.c
@@ -0,0 +1,8 @@
+#include "dbase_api_obj.h"
+
+int DB_get_val32(int db_elem, int instance, unsigned int *val)
+{
+	*val = 1;
+	
+	return 0;
+}
---
+++ websocketserver/dbase_api_obj.h
@@ -0,0 +1,7 @@
+#ifndef __dbase_api_obj__
+#define __dbase_api_obj__
+
+
+#define DB_ELEM_CHAT_COMM_MODE 1
+
+#endif
